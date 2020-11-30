fe2756a9a86adff89ebe9dd4260eaa700b87114e
GabrielMato <58472077+GabrielMatoBoopetyScoopWoop@users.noreply.github.com>
2020-11-30 09:23:34 +0100

    Add files via upload

---
+++ prosjekOcjena/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace prosjekOcjena
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UnosButun = new System.Windows.Forms.Button();
+            this.Unos = new System.Windows.Forms.TextBox();
+            this.Ispis = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // UnosButun
+            // 
+            this.UnosButun.Location = new System.Drawing.Point(418, 161);
+            this.UnosButun.Name = "UnosButun";
+            this.UnosButun.Size = new System.Drawing.Size(109, 23);
+            this.UnosButun.TabIndex = 0;
+            this.UnosButun.Text = "Upisi";
+            this.UnosButun.UseVisualStyleBackColor = true;
+            this.UnosButun.Click += new System.EventHandler(this.UnosButun_Click);
+            // 
+            // Unos
+            // 
+            this.Unos.Location = new System.Drawing.Point(312, 161);
+            this.Unos.Multiline = true;
+            this.Unos.Name = "Unos";
+            this.Unos.Size = new System.Drawing.Size(100, 23);
+            this.Unos.TabIndex = 1;
+            // 
+            // Ispis
+            // 
+            this.Ispis.Location = new System.Drawing.Point(312, 216);
+            this.Ispis.Multiline = true;
+            this.Ispis.Name = "Ispis";
+            this.Ispis.Size = new System.Drawing.Size(215, 134);
+            this.Ispis.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(312, 121);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(215, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(309, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ocjena";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(309, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Prosjek Ocjena";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(309, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Predmet";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Ispis);
+            this.Controls.Add(this.Unos);
+            this.Controls.Add(this.UnosButun);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UnosButun;
+        private System.Windows.Forms.TextBox Unos;
+        private System.Windows.Forms.TextBox Ispis;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ prosjekOcjena/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prosjekOcjena
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double brojac, ocjene = 0;
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UnosButun_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Convert.ToInt32(Unos.Text) < 1 || Convert.ToInt32(Unos.Text) > 5)
+                {
+                    MessageBox.Show("Niste upisali broj od 1 do 5", "Greška");
+                }
+                else
+                {
+                    brojac++;
+                    ocjene += Convert.ToInt32(Unos.Text);
+                    Ispis.AppendText(ocjene + "\r\n");
+                }
+                DialogResult odgovor = MessageBox.Show("Želite li unesti novu ocjenu?", "Izbor", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (odgovor == DialogResult.Yes)
+                {
+                    Unos.Clear();
+                }
+                else
+                {
+                    Ispis.Text = "Prosjek ocjena je = " + (ocjene / brojac);
+                }
+            }
+            catch (Exception Pogreska)
+            {
+                MessageBox.Show("Niste upisali broj", "Greška");
+            }
+        }
+    }
+}
